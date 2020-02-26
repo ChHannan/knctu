c6dee47933dcec4eb0da623031d4992bfd287165
ChHannan <45265763+ChHannan@users.noreply.github.com>
2020-02-26 18:24:15 +0500

    Latest

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -54,7 +54,6 @@ Future<http.Response> getUserFromId(String id) {
   });
 }
 
-
 Future<http.Response> getChatRooms() {
   return http.get('${apiUrl}chat/room/', headers: {
     HttpHeaders.authorizationHeader: _token,
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,11 @@ import 'package:knctu/api/api.dart';
 import 'package:knctu/db/db.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  var x = await getQuestions();
+  print(x.body);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -26,7 +30,7 @@ class MyApp extends StatelessWidget {
       create: (_) => AppDB(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: CurrentRoute(),
+        home: LoginScreen(),
         theme: ThemeData(
           primaryColor: Color(0xFF19b7c6),
           pageTransitionsTheme: PageTransitionsTheme(
@@ -50,7 +54,7 @@ class CurrentRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     final db = Provider.of<AppDB>(context);
     return FutureBuilder(
-      future: db.userDao.getLoggedInUser(),
+      future: db.userDao.getLoggedInUser().timeout(Duration(seconds: 5)),
       builder: (context, snapshot) {
         if (snapshot.hasData && !snapshot.hasError) {
           if (snapshot.data?.id != null) {
